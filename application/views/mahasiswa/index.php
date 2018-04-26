ce9d2c80f8a6839389e6aa3ed28259da8162c546
agungdh <agungdh@live.com>
2018-04-26 07:19:02 +0700

    update_test

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -14,6 +14,7 @@ $(document).ready(function() {
         "processing": true, //Feature control the processing indicator.
         "serverSide": true, //Feature control DataTables' server-side processing mode.
         "order": [], //Initial no order.
+        "scrollX": false,
  
         // Load data for the table's content from an Ajax source
         "ajax": {
